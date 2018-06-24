f6d2765aeb3885d18d8918cfc850807b0c5dc128
ktanaka117 <key.m4096.kung.fu@gmail.com>
2018-06-24 19:06:55 +0900

    データモデルを定義

---
+++ 06/MVPSample/MVPSample/Model/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  MVPSample
+//
+//  Created by 田中賢治 on 2018/06/24.
+//  Copyright © 2018年 田中賢治. All rights reserved.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let fullName: String
+    let numberOfIssues: Int
+    let numberOfForks: Int
+
+    private enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+        case numberOfIssues = "open_issues_count"
+        case numberOfForks = "forks_count"
+    }
+}
