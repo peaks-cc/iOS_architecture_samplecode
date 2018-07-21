d95e2e664fde3ce2a1c98a1cc9b9a3f5a3018d43
ktanaka117 <>
2018-07-21 18:20:09 +0900

    RepositoryListPresenterに必要なインターフェースを定義した

---
+++ 06/SupervisingControllerSample/SupervisingControllerSample/Presenter/RepositoryListPresenter.swift
@@ -0,0 +1,25 @@
+//
+//  RepositoryListPresenter.swift
+//  SupervisingControllerSample
+//
+//  Created by Kenji Tanaka on 2018/07/21.
+//  Copyright © 2018年 Kenji Tanaka. All rights reserved.
+//
+
+import Foundation
+
+class RepositoryListPresenter {
+    var repositories: [Repository] = []
+
+    var numberOfRepositories: Int {
+        return 0
+    }
+
+    func inject(repositoryListViewController: RepositoryListViewController) {
+        
+    }
+
+    func viewDidLoad() {
+        
+    }
+}
