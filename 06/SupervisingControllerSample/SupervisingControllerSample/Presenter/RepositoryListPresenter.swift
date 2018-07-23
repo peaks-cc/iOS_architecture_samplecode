c35c61d7d51f619e62162a14dc132c6b7196132e
ktanaka117 <>
2018-07-23 13:30:32 +0900

    データバインディングを実装

--- 06/SupervisingControllerSample/SupervisingControllerSample/Presenter/RepositoryListPresenter.swift
+++ 06/SupervisingControllerSample/SupervisingControllerSample/Presenter/RepositoryListPresenter.swift
@@ -9,17 +9,37 @@
 import Foundation
 
 class RepositoryListPresenter {
-    var repositories: [Repository] = []
+    private weak var repositoryListViewController: RepositoryListViewController!
+    private let gitHubClient = GitHubClient()
+
+    var repositories: [Repository] = [] {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.repositoryListViewController.reload()
+            }
+        }
+    }
 
     var numberOfRepositories: Int {
-        return 0
+        return repositories.count
     }
 
     func inject(repositoryListViewController: RepositoryListViewController) {
-        
+        self.repositoryListViewController = repositoryListViewController
     }
 
     func viewDidLoad() {
-        
+        getRepositoryList()
+    }
+
+    private func getRepositoryList() {
+        gitHubClient.fetchRepositoryList { [weak self] result in
+            switch result {
+            case .success(let repositories):
+                self?.repositories = repositories
+            case .error(let error):
+                ()
+            }
+        }
     }
 }
