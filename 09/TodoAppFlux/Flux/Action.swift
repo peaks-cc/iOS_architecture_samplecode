ee9f10f4f2cb219d97d5f65ebd4eecfee4344853
marty-suzuki <s1180183@gmail.com>
2018-08-06 14:46:15 +0900

    FluxのTodoのサンプルアプリを削除

--- 09/TodoAppFlux/Flux/Action.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Action.swift
-//  Flux
-//
-//  Created by marty-suzuki on 2018/07/01.
-//  Copyright © 2018年 marty-suzuki. All rights reserved.
-//
-
-public protocol Action {}
--- 09/TodoAppFlux/README.md
+++
@@ -1,1 +0,0 @@
-# flux-todomvcのSwift版
