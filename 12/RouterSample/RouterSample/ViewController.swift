3838f56941ebcbad07a7596dc7085880c441728a
ktanaka117 <>
2018-09-23 16:38:39 +0900

    Routerのサンプルプロジェクトを作成

---
+++ 12/RouterSample/RouterSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RouterSample
+//
+//  Created by Kenji Tanaka on 2018/09/23.
+//  Copyright © 2018年 Kenji Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
