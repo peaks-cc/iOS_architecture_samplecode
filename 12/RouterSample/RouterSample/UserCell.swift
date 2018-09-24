0cd95ebcf75e2dad59f76f29162aeda5e7b619aa
ktanaka117 <>
2018-09-24 19:42:30 +0900

    cellにimageがセットされてなかったのを直したり、reuseに備えたり

--- 12/RouterSample/RouterSample/UserCell.swift
+++ 12/RouterSample/RouterSample/UserCell.swift
@@ -13,16 +13,38 @@ class UserCell: UITableViewCell {
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
 
+    private var task: URLSessionTask?
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        task?.cancel()
+        task = nil
+        imageView?.image = nil
+    }
+
     func configure(user: User) {
-        let url = user.avatarURL
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let imageData = data {
-                DispatchQueue.main.async { [weak self] in
-                    self?.imageView?.image = UIImage(data: imageData)
+        task = {
+            let url = user.avatarURL
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let imageData = data else {
+                    return
+                }
+
+                DispatchQueue.global().async { [weak self] in
+                    guard let image = UIImage(data: imageData) else {
+                        return
+                    }
+
+                    DispatchQueue.main.async {
+                        self?.imageView?.image = image
+                        self?.setNeedsLayout()
+                    }
                 }
             }
-        }
-        task.resume()
+            task.resume()
+            return task
+        }()
 
         nameLabel.text = user.login
     }
