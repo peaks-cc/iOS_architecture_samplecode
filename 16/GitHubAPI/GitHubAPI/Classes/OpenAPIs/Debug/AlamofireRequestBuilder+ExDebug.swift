f7f36ff854cf1979f37707717e0d4dfeae514bfb
Yohei Sugigami <y.sugigami@gmail.com>
2018-10-31 01:52:52 +0900

    Replace cURL log logic

--- 16/GitHubAPI/GitHubAPI/Classes/OpenAPIs/Debug/AlamofireRequestBuilder+ExDebug.swift
+++ 16/GitHubAPI/GitHubAPI/Classes/OpenAPIs/Debug/AlamofireRequestBuilder+ExDebug.swift
@@ -3,8 +3,14 @@ import Alamofire
 
 #if DEBUG
 extension AlamofireRequestBuilder {
+    func createURLRequest() -> URLRequest? {
+        let encoding: ParameterEncoding = isBody ? JSONDataEncoding() : URLEncoding()
+        guard let originalRequest = try? URLRequest(url: URLString, method: HTTPMethod(rawValue: method)!, headers: buildHeaders()) else { return nil }
+        return try? encoding.encode(originalRequest, with: parameters)
+    }
+
     public var cURLString: String {
-        return makeDataRequest().request?.cURLString ?? ""
+        return createURLRequest()?.cURLString ?? ""
     }
 }
 #endif
