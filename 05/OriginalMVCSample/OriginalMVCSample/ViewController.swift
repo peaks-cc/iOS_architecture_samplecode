dd1c00e2ce942e8a7cb6103036e914c0e2dce77a
星野恵瑠 <shonetesmith@gmail.com>
2018-11-07 00:30:30 +0900

    CocoaMVCSampleとOriginalMVCSampleのプロジェクトをまずテンプレから作る

---
+++ 05/OriginalMVCSample/OriginalMVCSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OriginalMVCSample
+//
+//  Created by 史翔新 on 2018/11/07.
+//  Copyright © 2018年 史翔新. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
