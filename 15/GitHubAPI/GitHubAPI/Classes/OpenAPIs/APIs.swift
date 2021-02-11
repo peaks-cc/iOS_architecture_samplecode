6cd100eed7f0c370c48034f82d802629ba44b0e0
takasek <takassekiyoshi@gmail.com>
2021-02-11 22:28:43 +0900

    15章のSwift5化

--- 15/GitHubAPI/GitHubAPI/Classes/OpenAPIs/APIs.swift
+++ 15/GitHubAPI/GitHubAPI/Classes/OpenAPIs/APIs.swift
@@ -7,10 +7,10 @@
 import Foundation
 
 open class GitHubAPIAPI {
-    open static var basePath = "https://api.github.com/"
-    open static var credential: URLCredential?
-    open static var customHeaders: [String:String] = [:]
-    open static var requestBuilderFactory: RequestBuilderFactory = AlamofireRequestBuilderFactory()
+    public static var basePath = "https://api.github.com/"
+    public static var credential: URLCredential?
+    public static var customHeaders: [String:String] = [:]
+    public static var requestBuilderFactory: RequestBuilderFactory = AlamofireRequestBuilderFactory()
 }
 
 open class RequestBuilder<T> {
